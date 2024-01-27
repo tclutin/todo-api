7d1849088580ffaf95193aab13e8d5eec12e5b1e
Lutin <tclutin01@gmail.com>
2024-01-27 22:25:07 +0500

    add: logger init

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"log/slog"
+	"os"
 	"todo/internal/config"
 )
 
 func main() {
 	//Initializing the config
 	cfg := config.MustLoad()
+
+	//Initializing the logger
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
 }
